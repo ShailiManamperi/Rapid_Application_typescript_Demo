5d2f88c19ee4072a8faaee62da92ff736562f5a2
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-25 12:38:00 +0530

    add object ts & js

---
+++ 4_objects.js
@@ -0,0 +1,4 @@
+//objects
+var student;
+student = { id: 1, name: 'shaili', age: 21, college: 'ijse' };
+console.log(student);
---
+++ 4_objects.ts
@@ -0,0 +1,12 @@
+//objects
+
+let student: {
+    id: number;
+    name: string;
+    age: number;
+    college: string;
+};
+
+student ={id:1,name:'shaili',age:21,college:'ijse'}
+
+console.log(student);
